4f7a417929c0ba4d66f260102d40c381e48a2d3e
Moud <mahmoudaburascea@gmail.com>
2021-12-28 02:07:30 +0200

    FEATURE: created chains component with automatic fetching.

---
+++ src/views/chains/chains.container.tsx
@@ -0,0 +1,62 @@
+import { GridColDef } from "@mui/x-data-grid";
+import React from "react";
+import {
+    useBlockTransactions,
+    useGetBlockNumber,
+    useSelectedChainId,
+} from "../../hooks/chainDataHooks";
+import { ChainsPresent } from "./chains.present";
+
+export const ChainsContainer = () => {
+    const getBlockNumber = useGetBlockNumber();
+    const getBlockTransactions = useBlockTransactions();
+
+    const [blockNumber, setBlockNumber] = React.useState<number>();
+    const [blockData, setBlockData] = React.useState<number>();
+    const [txModalState, setTxModalState] = React.useState<boolean>(false);
+
+    const chainId = useSelectedChainId();
+
+    const dataTableColumns: GridColDef[] = [
+        { field: 'hash', headerName: 'Hash', width: 250 },
+        { field: 'confirmations', headerName: 'Confirmations', width: 140 },
+        { field: 'gasPrice', headerName: 'Gas Price', width: 150 },
+        { field: 'value', headerName: 'Value', width: 150 },
+        { field: 'from', headerName: 'From', width: 250 },
+        { field: 'to', headerName: 'To', width: 250 },
+    ]
+    const buildTableRows = (transactionData: any[]) => {
+        const transactions = transactionData.map((transaction, index) => ({
+            id: index,
+            hash: transaction.hash,
+            confirmations: transaction.confirmations,
+            nonce: transaction.nonce,
+            gasPrice: transaction.gasPrice.toString(),
+            value: transaction.value.toString(),
+            from: transaction.from,
+            to: transaction.to,
+        }))
+        console.log("transactions ", transactions);
+        return transactions;
+    }
+
+    React.useEffect(() => {
+        getBlockNumber()
+            .then((response: any) => setBlockNumber(response));
+    }, []);
+
+    React.useEffect(() => {
+        if(!blockNumber) return;
+        getBlockTransactions(blockNumber)
+            .then((response: any) => setBlockData(response))
+    }, [blockNumber]);
+
+    return <ChainsPresent
+                chainId={chainId}
+                blockNumber={blockNumber}
+                blockData={blockData}
+                txModalState={txModalState}
+                setTxModalState={setTxModalState}
+                dataTableColumns={dataTableColumns}
+                buildTableRows={buildTableRows}/>;
+};
---
+++ src/views/chains/chains.present.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Grid, Box, Typography, Modal, Button, Card, LinearProgress } from "@mui/material";
+import { networks } from "../../hooks/useGetProvider";
+import './chains.styles.scss';
+import { DataGrid } from "@mui/x-data-grid";
+
+const modalStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 1200,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+  };
+
+export const ChainsPresent = ({ chainId, blockNumber, blockData, txModalState, setTxModalState, dataTableColumns, buildTableRows }: any) => {
+  return (
+    <Box sx={{ padding: "20px" }}>
+      <Grid container spacing={2}>
+        <Grid sm={12} xs={12}>
+          <Card sx={{ padding: 2, margin: 2 }}>
+            <Typography variant="h3">🔗 Chain Info</Typography>
+            <Typography variant="h4">{networks[chainId]?.label ?? 'Chain Name...'}</Typography>
+          </Card>
+        </Grid>
+        <Grid sm={6} xs={12}>
+          <Card sx={{ padding: 2, margin: 2 }}>
+            <Typography variant="subtitle1">💠 Blockhain Id</Typography>
+            <Typography variant="h5">{chainId ?? <LinearProgress sx={{margin: 2}} color="primary" />}</Typography>
+          </Card>
+        </Grid>
+        <Grid sm={6} xs={12}>
+          <Card sx={{ padding: 2, margin: 2 }}>
+            <Typography variant="subtitle1">💠 Latest Block Number</Typography>
+            <Typography variant="h5">{blockNumber ?? <LinearProgress sx={{margin: 2}} color="primary" />}</Typography>
+          </Card>
+        </Grid>
+        <Grid sm={6} xs={12}>
+          <Card sx={{ padding: 2, margin: 2 }}>
+            <Typography variant="subtitle1">💠 Miner Address</Typography>
+            <Typography variant="h6">{blockData?.miner ?? <LinearProgress sx={{margin: 2}} color="primary" />}</Typography>
+          </Card>
+        </Grid>
+        <Grid sm={6} xs={12}>
+          <Card sx={{ padding: 2, margin: 2 }}>
+            <Typography variant="subtitle1">💠 Current Block Time</Typography>
+            <Typography variant="h5">{blockData?.timestamp ? new Date(blockData.timestamp * 1000).toDateString() : <LinearProgress sx={{margin: 2}} color="primary" />}</Typography>
+          </Card>
+        </Grid>
+        <Grid sm={6} xs={12}>
+          <Card sx={{ padding: 2, margin: 2 }}>
+            <Typography variant="subtitle1">💠 Block Difficulty</Typography>
+            <Typography variant="h6">{blockData?._difficulty ? blockData._difficulty.toString() : <LinearProgress sx={{margin: 2}} color="primary" />}</Typography>
+          </Card>
+        </Grid>
+        <Grid sm={6} xs={12}>
+          <Card sx={{ padding: 2, margin: 2 }}>
+            <Typography variant="subtitle1">💠 Transactions</Typography>
+                {blockData?.transactions ? (
+                  <Button variant="contained" onClick={() => setTxModalState(true)}>
+                    {blockData.transactions.length} transactions, Click to view
+                  </Button>)  : <LinearProgress sx={{margin: 2}} color="primary" />}
+          </Card>
+        </Grid>
+      </Grid>
+
+        <Modal open={txModalState} onClose={() => setTxModalState(false)}>
+            {blockData?.transactions ?
+                (<Box sx={modalStyle}>
+                    <Typography variant="h6" component="h2">
+                        {buildTableRows(blockData?.transactions).length} Transactions 
+                    </Typography>
+                    <Typography variant="subtitle1">💠 
+                        Click Column Header to sort
+                    </Typography>
+                        
+                    <div style={{ height: 500, width: '100%' }}>
+                        <DataGrid rows={buildTableRows(blockData?.transactions)} columns={dataTableColumns} />
+                    </div>
+                </Box>) : (
+                    <Typography variant="h6" component="h2">
+                        Transaction List Is Loading...
+                    </Typography>
+                )}
+        </Modal>
+    </Box>
+  );
+};
