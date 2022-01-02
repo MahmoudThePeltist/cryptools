1e17ea55020c500073323032a688cec59d02dab6
Moud <mahmoudaburascea@gmail.com>
2022-01-02 20:25:58 +0200

    FEATURE: added new event tracking functionality and management to tokens screen, as well as default tokens.

--- src/views/tokens/tokens.container.tsx
+++ src/views/tokens/tokens.container.tsx
@@ -1,18 +1,52 @@
+import { useState } from '@hookstate/core';
 import { ethers } from 'ethers';
 import React from 'react';
-import { useGetTokenContract, useGetTokenData } from '../../hooks/tokenHooks';
+import { globalState } from '../../hooks/stateHooks';
+import {
+    useGetTokenData,
+    useSubscribeEvent,
+    useUnsubscribeAllListeners,
+    useUnsubscribeListener} from '../../hooks/tokenHooks';
+import { useChainData } from '../../hooks/useGetProvider';
+import { IApproval, ITokenData, ITransfer } from '../../interfaces/token.intefaces';
+import { ExportModalContainer } from './exportModal/exportModal.container';
 import { TokensPresent } from "./tokens.present"
 
 export const TokensContainer = () => {
 
+    const state = useState(globalState);
+
     const [loading, setLoading] = React.useState<boolean>(false);
     const [error, setError] = React.useState<string>();
-    
     const [address, setAddress] = React.useState<string>();
     const [addressValid, setAddressValid] = React.useState<boolean>(false);
-    const [tokenData, setTokenData] = React.useState<any>();
+    const [tokenData, setTokenData] = React.useState<ITokenData>();
+    const [openModal, setOpenModal] = React.useState<boolean>(false);
+    const [exportVariant, setExportVariant] = React.useState<string>();
+
+    const networkData = useChainData();
     const getTokenData = useGetTokenData();
-    
+    const subscribeToEvent = useSubscribeEvent();
+    const unsubscribe = useUnsubscribeListener();
+    const unsubscribeAll = useUnsubscribeAllListeners();
+    const resetTransfers = () => state.set(data => ({...data, transfers: []}));
+    const resetApprovals = () => state.set(data => ({...data, approvals: []}));
+    const resetActiveEvents = () => state.set(data => ({...data, activeEvents: []}));
+
+    // Validate the address
+    React.useEffect(() => setAddressValid(ethers.utils.isAddress(String(address))), [address]);
+
+    React.useEffect(() => {
+        resetTransfers();
+        resetApprovals();
+        resetActiveEvents();
+    }, [tokenData, networkData])
+
+    const openExportModal = (variant: string): void => {
+        setExportVariant(variant);
+        setOpenModal(true);
+    }
+
     const fetchTokenData = () => {
         setError(undefined);
         setTokenData(undefined);
@@ -28,18 +62,82 @@ export const TokensContainer = () => {
         }
     }
 
-    // Validate the address
-    React.useEffect(() => setAddressValid(ethers.utils.isAddress(String(address))), [address]);
+    const handleNewTransfer = (from: any, to: any, value: any, ...other: any) => {
+        if(state.value?.transfers.length > state.value.listenerCap || !state.value.activeEvents.includes("Transfer")) return;
+        let transfer: ITransfer = {to, from, value: value.toString()};
+        state.set(data => ({...data, transfers: [...data.transfers, transfer]}));
+    }
+
+    const handleNewApproval = (owner: any, spender: any, value: any, ...other: any) => {
+        if(state.value?.approvals.length > state.value.listenerCap || !state.value.activeEvents.includes("Approval")) return;
+        let approval: IApproval = {owner, spender, value: value.toString()};
+        state.set(data => ({...data, approvals: [...data.approvals, approval]}));
+    }
+
+    const listenForTransfer = () => {
+        let event = "Transfer";
+        resetTransfers();
+
+        if(address && addressValid) {
+            if(state.value.activeEvents.includes(event)) return;
+            state.set(data => ({...data, activeEvents: [...data.activeEvents, event]}));
+            subscribeToEvent(event, address, handleNewTransfer);
+        }
+    }
+
+    const listenForApproval = () => {
+        let event = "Approval";
+        resetApprovals();
+
+        if(address && addressValid) {
+            if(state.value.activeEvents.includes(event)) return;
+            state.set(data => ({...data, activeEvents:  [...data.activeEvents, event]}));
+            subscribeToEvent(event, address, handleNewApproval);
+        }
+    }
+
+    const stopListeningForEvent = (event: string) => {        
+        if(!state.value.activeEvents.includes(event) || !address) return;
+        state.set(data => ({...data, activeEvents: data.activeEvents.filter(e => e != event)}));
+        
+        if(event == "Transfer")
+            unsubscribe(event, handleNewTransfer, address)
+                .then(() => console.log(`Unsubscribe: ${event}`));
+        if(event == "Approval")
+            unsubscribe(event, handleNewApproval, address)
+                .then(() => console.log(`Unsubscribe: ${event}`));
+        
+    }
 
     return (
-        <TokensPresent
-            address={address}
-            setAddress={setAddress}
-            loading={loading}
-            addressValid={addressValid}
-            tokenData={tokenData}
-            fetchTokenData={fetchTokenData}
-            error={error}
-        />
+        <>
+            <TokensPresent
+                address={address}
+                setAddress={setAddress}
+                addressValid={addressValid}
+                tokenData={tokenData}
+                fetchTokenData={fetchTokenData}
+
+                listenForTransfer={listenForTransfer}
+                listenForApproval={listenForApproval}
+                stopListeningForEvent={stopListeningForEvent}
+                activeEvents={state.value.activeEvents}
+                stateData={state.value}
+                networkData={networkData}
+                listenerCap={state.value.listenerCap}
+                setListenerCap={(value: number) => state.set(data => ({...data, listenerCap: value}))}
+
+                openExportModal={openExportModal}
+                loading={loading}
+                error={error}
+            />
+
+            <ExportModalContainer
+                tokenData={tokenData}
+                open={openModal}
+                exportVariant={exportVariant}
+                setOpen={setOpenModal}/>
+
+        </>
     )
 }
--- src/views/tokens/tokens.present.tsx
+++ src/views/tokens/tokens.present.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import { Grid, Box, Typography, Card, TextField, Button, LinearProgress } from '@mui/material';
+import { CSVLink } from "react-csv";
+import { IDefaultToken } from '../../utils/networkDefinitions.utils';
+import "./tokens.styles.scss";
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import HearingIcon from '@mui/icons-material/Hearing';
+import HearingDisabledIcon from '@mui/icons-material/HearingDisabled';
 
-export const TokensPresent = ({address, setAddress, loading, addressValid, fetchTokenData, tokenData, error}: any) => {
+export const TokensPresent = ({
+        address,
+        setAddress,
+        addressValid,
+        tokenData,
+        fetchTokenData,
+
+        listenForTransfer,
+        listenForApproval,
+        stopListeningForEvent,
+        activeEvents,
+        stateData,
+        networkData,    
+        listenerCap,
+        setListenerCap,
+
+        openExportModal,
+        loading,
+        error,
+    }: any) => {
 
     return (
         <Box sx={{padding: '20px'}}>
@@ -9,7 +34,7 @@ export const TokensPresent = ({address, setAddress, loading, addressValid, fetch
                 <Grid xs={12}>
                     <Card sx={{padding: 2, margin: 2}}>
                         <Typography variant='h3'>Token Info</Typography>
-                        <Typography variant='h4'>Fetch and display token data</Typography>
+                        <Typography variant='h4'>Fetch token data from {networkData.label}</Typography>
                     </Card>
                 </Grid>
                 <Grid xs={12}>
@@ -36,6 +61,16 @@ export const TokensPresent = ({address, setAddress, loading, addressValid, fetch
                                 </Button>
                             </Grid>
                             <Grid xs={12}>
+                                {networkData?.default_tokens?.map((data: IDefaultToken) => (
+                                    <Button
+                                        variant="contained" color="secondary"
+                                        sx={{marginTop: 1, marginRight: 1}}
+                                        onClick={() => setAddress(data.address)}>
+                                            { data.name }
+                                    </Button>
+                                ))}
+                            </Grid>
+                            <Grid xs={12}>
                                 {loading ? <LinearProgress sx={{margin: 2}} color="primary" /> : ''}
                             </Grid>
                         </Grid>
@@ -54,24 +89,80 @@ export const TokensPresent = ({address, setAddress, loading, addressValid, fetch
                 {tokenData ? <Grid xs={12}>
                     <Card sx={{padding: 2, margin: 2}}>
                         <Grid container>
-                            <Grid xs={4} sx={{padding: 2}}>
-                                <Typography variant='subtitle1'>Name</Typography>
-                                <Typography variant='h6'>{ tokenData.name }</Typography>
+                            <Grid xs={12} sx={{padding: 1}}>
+                                <Typography variant='h5'>General Details</Typography>
                             </Grid>
-                            <Grid xs={4} sx={{padding: 2}}>
-                                <Typography variant='subtitle1'>Symbol</Typography>
-                                <Typography variant='h6'>{ tokenData.symbol }</Typography>
+                            <Grid xs={3} sx={{padding: 1}}>
+                                <Card sx={{padding: 1}}>
+                                    <Typography variant='subtitle1'>Name</Typography>
+                                    <Typography variant='h6'>{ tokenData.name }</Typography>
+                                </Card>
                             </Grid>
-                            <Grid xs={4} sx={{padding: 2}}>
-                                <Typography variant='subtitle1'>Decimals</Typography>
-                                <Typography variant='h6'>{ tokenData.decimals }</Typography>
+                            <Grid xs={3} sx={{padding: 1}}>
+                                <Card sx={{padding: 1}}>
+                                    <Typography variant='subtitle1'>Symbol</Typography>
+                                    <Typography variant='h6'>{ tokenData.symbol }</Typography>
+                                </Card>
                             </Grid>
-                            <Grid xs={12} sx={{padding: 2}}>
-                                <Typography variant='subtitle1'>Total Tokens</Typography>
+                            <Grid xs={3} sx={{padding: 1}}>
+                                <Card sx={{padding: 1}}>
+                                    <Typography variant='subtitle1'>Decimals</Typography>
+                                    <Typography variant='h6'>{ tokenData.decimals }</Typography>
+                                </Card>
+                            </Grid>
+                            <Grid xs={3} sx={{padding: 1}}>
+                                <Card sx={{padding: 1}}>
+                                    <Typography variant='subtitle1'>Total Tokens</Typography>
+                                    <Typography variant='h6'>
+                                        { `${tokenData.totalSupply.toString() / (10 ** +tokenData.decimals)}` }
+                                    </Typography>
+                                </Card>
+                            </Grid>
+                            <Grid xs={12} sx={{padding: 1, marginTop: 2}}>
+                                <Typography variant='h5'>Listen To Events</Typography>
                                 <Typography variant='h6'>
-                                    { `${tokenData.totalSupply.toString() / (10 ** +tokenData.decimals)}` }
+                                    Transactions capped to
+                                    <TextField
+                                        type="number"
+                                        sx={{paddingLeft: 1, paddingRight: 1, maxWidth: 60}}
+                                        variant="standard"
+                                        value={listenerCap}
+                                        onChange={(e) => setListenerCap(e.target.value)}/>
+                                    to avoid leaks
                                 </Typography>
                             </Grid>
+                            <Grid xs={12} sx={{padding: 1}}>
+                                <Button
+                                    variant="contained"
+                                    sx={{marginRight: 1}}
+                                    onClick={() => (!activeEvents.includes("Transfer") ? listenForTransfer() : stopListeningForEvent("Transfer"))}>
+                                        { !activeEvents.includes("Transfer") ? (<><HearingIcon/> Transfer</>) : (<><HearingDisabledIcon/> Transfer</>) }
+                                </Button>
+                                
+                                <Button
+                                    variant="contained" color="secondary"
+                                    sx={{marginRight: 1}}
+                                    onClick={() => openExportModal("Transfers")}>
+                                    <FileDownloadIcon/> {stateData?.transfers?.length} Transfers
+                                </Button>
+                            </Grid>
+                            <Grid xs={12} sx={{padding: 1}}>
+                                <Button
+                                    variant="contained"
+                                    sx={{marginRight: 1}}
+                                    onClick={() => (!activeEvents.includes("Approval") ? listenForApproval() : stopListeningForEvent("Approval"))}>
+                                        { !activeEvents.includes("Approval") ? (<><HearingIcon/> Approval</>) : (<><HearingDisabledIcon/> Approval</>) }
+                                </Button>
+                                
+                                <Button
+                                    variant="contained" color="secondary"
+                                    sx={{marginRight: 1}}
+                                    onClick={() => openExportModal("Approvals")}>
+                                    <FileDownloadIcon/> {stateData?.approvals?.length} Approvals
+                                </Button>
+                            </Grid>
+
+                            
                         </Grid>
                     </Card>
                 </Grid> : undefined}
