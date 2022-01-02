f4800794cbd4c9a371cbd01b76c6b81f6a1f85d2
Moud <mahmoudaburascea@gmail.com>
2022-01-02 20:20:15 +0200

    MODIFICATION: expanded managed state.

--- src/hooks/stateHooks.tsx
+++ src/hooks/stateHooks.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { createState } from '@hookstate/core';
 import { Persistence } from '@hookstate/persistence';
+import { IApproval, ITransfer } from '../interfaces/token.intefaces';
 
-export const globalState = createState({
-    activeChainId: 1
-});
+export interface IState {
+    activeChainId: number,
+    listenerCap: number,
+    transfers: ITransfer[], 
+    approvals: IApproval[], 
+    activeEvents: string[],
+}
+
+export const defaultState: IState = {
+    activeChainId: 1,
+    listenerCap: 500,
+    transfers: [],
+    approvals: [],
+    activeEvents: []
+}
+
+export const globalState = createState(defaultState);
 
 globalState.attach(Persistence('plugin-persisted-data-key'));
