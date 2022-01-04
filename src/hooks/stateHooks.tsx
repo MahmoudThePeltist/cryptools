a96b336d00dbc3af93ae0139be8ab1a41cb46094
Moud <mahmoudaburascea@gmail.com>
2022-01-04 12:30:36 +0200

    FEATURE: new token state.

--- src/hooks/stateHooks.tsx
+++ src/hooks/stateHooks.tsx
@@ -3,22 +3,35 @@ import { createState } from '@hookstate/core';
 import { Persistence } from '@hookstate/persistence';
 import { IApproval, ITransfer } from '../interfaces/token.intefaces';
 
-export interface IState {
+export interface IGlobalState {
     activeChainId: number,
     listenerCap: number,
+    throttleCap: number,
     transfers: ITransfer[], 
     approvals: IApproval[], 
     activeEvents: string[],
 }
+export interface ITokenState {
+    testedAddresses: {address: string, status: boolean}[],
+    uniqueAddresses: string[],
+}
 
-export const defaultState: IState = {
+export const defaultState: IGlobalState = {
     activeChainId: 1,
     listenerCap: 500,
+    throttleCap: 1,
     transfers: [],
     approvals: [],
     activeEvents: []
 }
 
+export const defaultTokenState: ITokenState = {
+    testedAddresses: [],
+    uniqueAddresses: [],
+}
+
 export const globalState = createState(defaultState);
+
+export const tokenState = createState(defaultTokenState);
 
 globalState.attach(Persistence('plugin-persisted-data-key'));
