910036d819de934a1fb20189e4ca11212690c994
Moud <mahmoudaburascea@gmail.com>
2022-01-24 01:05:43 +0200

    FEATURE: expanded chains component with new feature to predict blocknumbers based on current blocknumber and average blocktime.

--- src/views/chains/chains.container.tsx
+++ src/views/chains/chains.container.tsx
@@ -6,16 +6,30 @@ import {
 } from "../../hooks/chainDataHooks";
 import { useSelectedChainId } from "../../hooks/useSelectedChainId";
 import { ChainsPresent } from "./chains.present";
+import { networks } from "../../utils/networkDefinitions.utils";
 
 export const ChainsContainer = () => {
     const getBlockNumber = useGetBlockNumber();
     const getBlockTransactions = useBlockTransactions();
+    const chainId = useSelectedChainId();
 
     const [blockNumber, setBlockNumber] = React.useState<number>();
     const [blockData, setBlockData] = React.useState<number>();
     const [txModalState, setTxModalState] = React.useState<boolean>(false);
-
-    const chainId = useSelectedChainId();
+    const [calcAvgBlocktime, setCalcAvgBlocktime] = React.useState<number>(networks[chainId]?.avg_blocktime);
+    const [calcMiningDate, setCalcMiningDate] = React.useState<Date>(new Date());
+    const [calcBlockNumber, setCalcBlockNumber] = React.useState<number>();
+    
+    React.useEffect(() => {
+        let targetTime: number = calcMiningDate.getTime();
+        let currentTime: number = new Date().getTime();
+        let timeDifferenceInSecs: number = (targetTime - currentTime) / 1000;
+        if(blockNumber) {
+            const newBlockNumber: number = Math.round(blockNumber + (timeDifferenceInSecs / calcAvgBlocktime));
+            setCalcBlockNumber(newBlockNumber);
+        }
+    }, [calcAvgBlocktime, calcMiningDate]);
+    
 
     const dataTableColumns: GridColDef[] = [
         { field: 'hash', headerName: 'Hash', width: 250 },
@@ -54,10 +68,17 @@ export const ChainsContainer = () => {
 
     return <ChainsPresent
                 chainId={chainId}
+                network={networks[chainId]}
                 blockNumber={blockNumber}
                 blockData={blockData}
                 txModalState={txModalState}
                 setTxModalState={setTxModalState}
                 dataTableColumns={dataTableColumns}
-                buildTableRows={buildTableRows}/>;
+                buildTableRows={buildTableRows}
+                calcAvgBlocktime={calcAvgBlocktime}
+                calcMiningDate={calcMiningDate}
+                calcBlockNumber={calcBlockNumber}
+                setCalcAvgBlocktime={setCalcAvgBlocktime}
+                setCalcMiningDate={setCalcMiningDate}
+                setCalcBlockNumber={setCalcBlockNumber}/>;
 };
--- src/views/chains/chains.present.tsx
+++ src/views/chains/chains.present.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Grid, Box, Typography, Modal, Button, Card, LinearProgress } from "@mui/material";
+import { Grid, Box, Typography, Modal, Button, Card, LinearProgress, TextField } from "@mui/material";
 import './chains.styles.scss';
 import { DataGrid } from "@mui/x-data-grid";
-import { networks } from "../../utils/networkDefinitions.utils";
+import { DateTimePicker } from "@mui/lab";
 
 const modalStyle = {
     position: 'absolute' as 'absolute',
@@ -15,14 +15,28 @@ const modalStyle = {
     p: 4,
   };
 
-export const ChainsPresent = ({ chainId, blockNumber, blockData, txModalState, setTxModalState, dataTableColumns, buildTableRows }: any) => {
+export const ChainsPresent = ({
+  chainId,
+  network,
+  blockNumber,
+  blockData,
+  txModalState,
+  setTxModalState,
+  dataTableColumns,
+  buildTableRows,
+  calcAvgBlocktime,
+  calcMiningDate,
+  calcBlockNumber,
+  setCalcAvgBlocktime,
+  setCalcMiningDate,
+  setCalcBlockNumber }: any) => {
   return (
     <Box sx={{ padding: "20px" }}>
       <Grid container spacing={2}>
         <Grid sm={12} xs={12}>
           <Card sx={{ padding: 2, margin: 2 }}>
             <Typography variant="h3">🔗 Chain Info</Typography>
-            <Typography variant="h4">{networks[chainId]?.label ?? 'Chain Name...'}</Typography>
+            <Typography variant="h4">{network?.label ?? 'Chain Name...'}</Typography>
           </Card>
         </Grid>
         <Grid sm={6} xs={12}>
@@ -62,6 +76,48 @@ export const ChainsPresent = ({ chainId, blockNumber, blockData, txModalState, s
                   <Button variant="contained" onClick={() => setTxModalState(true)}>
                     {blockData.transactions.length} transactions, Click to view
                   </Button>)  : <LinearProgress sx={{margin: 2}} color="primary" />}
+          </Card>
+        </Grid>
+        
+        <Grid sm={12} xs={12}>
+          <Card sx={{ padding: 2, margin: 2, marginBottom: 0 }}>
+            <Typography variant="h5">💠 Predict Block Number </Typography>
+            <Typography variant="subtitle2">Predict block number to be mined at specific date and time, assuming a specific average blocktime</Typography>
+            <Grid container>
+              
+              <Grid sm={4} xs={12} sx={{ padding: 2 }}>
+                <Typography variant="subtitle1">⌚ Average blocktime in seconds</Typography>
+                <TextField
+                  type="number"
+                  placeholder="Average blocktime in seconds"
+                  variant="standard"
+                  value={calcAvgBlocktime}
+                  onChange={(e: any) => setCalcAvgBlocktime(e.target.value)}/>
+              </Grid>
+              <Grid sm={4} xs={12} sx={{ padding: 2 }}>
+                <Typography variant="subtitle1">📅 Mining date</Typography>
+                <DateTimePicker
+                  renderInput={(props: any) => <TextField variant="standard" {...props} />}
+                  value={calcMiningDate}
+                  onChange={(value: any) => setCalcMiningDate(value)}>
+                </DateTimePicker>
+              </Grid>
+              <Grid sm={4} xs={12} sx={{ padding: 2 }}>
+                <Typography variant="subtitle1">🔢 Block Number</Typography>
+                <TextField
+                  type="number"
+                  placeholder="Target Block Number"
+                  variant="standard"
+                  value={calcBlockNumber}
+                  onChange={(e: any) => setCalcBlockNumber(e.target.value)}/>
+              </Grid>
+              <Typography variant="subtitle2">
+                Accurate average block time can be found at this chain's block explorer
+                {network.scanner_site ? <a href={network.scanner_site+'/chart/blocktime'}> 
+                    - go to explorer -
+                  </a> : undefined}
+              </Typography>
+            </Grid>
           </Card>
         </Grid>
       </Grid>
